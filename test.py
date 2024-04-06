0251d367699168aca5e8fe48977c4b36e534a760
MLS-22 <m7ammad22@gmail.com>
2024-04-06 17:10:18 +0200

    asd

--- test.py
+++ test.py
@@ -7,26 +7,32 @@ def download_ccminer(url):
 
 # Get CPU model information
 cpu_info = subprocess.check_output(["lscpu"]).decode("utf-8")
+cpu_model_line = [line for line in cpu_info.split('\n') if "Model name" in line]
 
-# Check CPU model and download ccminer accordingly
-if "Cortex-A53" in cpu_info and "Cortex-A72" in cpu_info:
-    download_ccminer("https://raw.githubusercontent.com/Darktron/pre-compiled/a73-a53/ccminer")
+if cpu_model_line:
+    cpu_model = cpu_model_line[0].split(":")[1].strip()
 
-elif "Cortex-A53" in cpu_info and "Cortex-A73" in cpu_info:
-    download_ccminer("https://raw.githubusercontent.com/Darktron/pre-compiled/a73-a53/ccminer")
+    # Check CPU model and download ccminer accordingly
+    if "Cortex-A53" in cpu_model and "Cortex-A72" in cpu_model:
+        download_ccminer("https://raw.githubusercontent.com/Darktron/pre-compiled/a73-a53/ccminer")
 
-elif "Cortex-A55" in cpu_info and "Cortex-A75" in cpu_info:
-    download_ccminer("https://raw.githubusercontent.com/Darktron/pre-compiled/a75-a55/ccminer")
+    elif "Cortex-A53" in cpu_model and "Cortex-A73" in cpu_model:
+        download_ccminer("https://raw.githubusercontent.com/Darktron/pre-compiled/a73-a53/ccminer")
 
-elif "Cortex-A53" in cpu_info:
-    download_ccminer("https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer")
+    elif "Cortex-A55" in cpu_model and "Cortex-A75" in cpu_model:
+        download_ccminer("https://raw.githubusercontent.com/Darktron/pre-compiled/a75-a55/ccminer")
 
-else:
-    print("Unsupported CPU model detected:", cpu_info)
-    choice = input("Do you want to download the generic ccminer? (y/n): ").lower()
-    if choice == 'y':
-        download_ccminer("https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer")
-    elif choice == 'n':
-        print("No action taken. Exiting.")
+    elif "Cortex-A53" in cpu_model:
+        download_ccminer("https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer")
+
     else:
-        print("Invalid choice. Exiting.")
+        print("Unsupported CPU model detected:", cpu_model)
+        choice = input("Do you want to download the generic ccminer? (y/n): ").lower()
+        if choice == 'y':
+            download_ccminer("https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer")
+        elif choice == 'n':
+            print("No action taken. Exiting.")
+        else:
+            print("Invalid choice. Exiting.")
+else:
+    print("Error: Could not find CPU model information.")
