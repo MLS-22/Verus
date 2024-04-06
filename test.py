6ae88a1f0fe144791c042d5adc878190fb1b7fbf
MLS-22 <m7ammad22@gmail.com>
2024-04-06 17:39:59 +0200

    Fix: code

--- test.py
+++ test.py
@@ -7,31 +7,24 @@ def download_ccminer(url):
     subprocess.run(["wget", url])
 
 # Get CPU model information
-cpu_info = subprocess.check_output(["lscpu"]).decode("utf-8")
-cpu_model_line = [line for line in cpu_info.split('\n') if "Model name" in line]
-
-if cpu_model_line:
-    cpu_model = cpu_model_line[0].split(":")[1].strip()
-
-    # Extract CPU numbers from the model
-    parts = cpu_model.split('Cortex-')
-    if len(parts) >= 3:  # Ensure there are enough parts to extract numbers
-        cpu_numbers = [part.split('-')[0].strip() for part in parts[1:]]
-
-        # Check if there are enough cpu_numbers to form the URL
-        if len(cpu_numbers) >= 2:
-            ccminer_url = f"https://raw.githubusercontent.com/Darktron/pre-compiled/{cpu_numbers[0]}-{cpu_numbers[1]}/ccminer"
-
-            # Check if ccminer URL exists
-            response = requests.head(ccminer_url)
-            if response.status_code == 200:
-                print("ccminer URL exists. Downloading...")
-                download_ccminer(ccminer_url)
-            else:
-                print("ccminer URL does not exist.")
-        else:
-            print("Error: Not enough CPU model numbers.")
+# cpu_info = subprocess.check_output(["lscpu"]).decode("utf-8")
+# cpu_model_line = [line for line in cpu_info.split('\n') if "Model name" in line]
+cpu_model_line = ['Model name:          Cortex-A53', 'Model name:          Cortex-A72']
+cpu_numbers = [line.split()[-1].split('-')[1].lower() for line in cpu_model_line]
+print(f'Your CPU: {cpu_numbers}", end="\n\n\n")
+      
+if len(cpu_numbers) > 0:
+    ccminer_url = f"https://raw.githubusercontent.com/Darktron/pre-compiled/{cpu_numbers[1]}-{cpu_numbers[0]}/ccminer"
+    response = requests.head(ccminer_url)
+    if response.status_code == 200:
+        print("ccminer URL exists. Downloading...")
+        download_ccminer(ccminer_url)
     else:
-        print("Error: Could not extract CPU model numbers.")
+        read = input("File not find, do you want to download default file ?")
+        if read == 'y':
+            ccminer_url = f"https://raw.githubusercontent.com/Darktron/pre-compiled/default/ccminer"
+            response = requests.head(ccminer_url)
+
+        print("ccminer URL does not exist.")
 else:
     print("Error: Could not find CPU model information.")
